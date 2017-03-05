ec0a3e00b62b6b1c72fe70823e7a5f25b3c12302
Useekaw <useekaw@gmail.com>
2017-03-05 20:05:15 +0100

    another change

--- fstest/fstest2.fs
+++ fstest/fstest2.fs
@@ -1,4 +1,16 @@
 type Status = Success=0 | Failure=1
-type Funds = { real:decimal; linked:decimal; promotion:decimal; }
+type Funds = { real:float; linked:float; promotion:float; }
 type FundsResult = { status:Status; contributive:bool; funds:Funds; }
 
+let funds = { real = 100.0; linked = 5.0; promotion = 10.0;}
+let order = -20.0
+
+let substract x y = x - y
+
+let xxx value funds =
+    { real = funds.real; linked = funds.linked; promotion = funds.promotion + order }
+
+
+let result = 
+    order
+    |> substract funds.promotion
